191cd875294aeb25827ff955259daa80c7761074
Christoph Heindl <christoph.heindl@gmail.com>
2016-01-05 10:34:23 +0100

    Attempting different methods for tracking wout VJ

--- examples/dest_track_video.cpp
+++ examples/dest_track_video.cpp
@@ -63,13 +63,15 @@ int main(int argc, char **argv)
         return -1;
     }
     
+    bool useRegressor2 = true;
+    /*
     const bool useRegressor2 = opts.regressor2 != "unused";
     if (useRegressor2 && !t[1].load(opts.regressor2)) {
         std::cerr << "Failed to load tier 2 regressor." << std::endl;
         return -1;
     } else if (!useRegressor2) {
         std::cout << "Not using tier 2 regressor. Using detector on every frame." << std::endl;
-    }
+    }*/
     
     dest::face::FaceDetector fd;
     if (!fd.loadClassifiers(opts.detector)) {
@@ -91,11 +93,17 @@ int main(int argc, char **argv)
         std::cerr << "Failed to open capture device." << std::endl;
         return -1;
     }
+
+    // The OpenCV detector rectangles are significantly different from tight bounds.
+    // The values below attempt to match tight rects and OpenCV rects.
+    float scaleToCV = 1.25f; // Scale between rectangles
+    float txToCV = -0.01f; // Translation in x normalized by image width
+    float tyToCV = -0.05f; // Translation in y normalized by image height
     
     cv::Mat imgCV, grayCV;
     cv::Rect cvRect;
     dest::core::Image img;
-    dest::core::Rect r;
+    dest::core::Rect r, r2;
     dest::core::Shape s;
     dest::core::ShapeTransform shapeToImage;
     bool done = false;
@@ -116,28 +124,42 @@ int main(int argc, char **argv)
                 dest::util::toDest(cvRect, r);
                 shapeToImage = dest::core::estimateSimilarityTransform(dest::core::unitRectangle(), r);
                 s = t[0].predict(img, shapeToImage);
-                r = dest::core::shapeBounds(s);
+
                 detect = false;
                 detectSuccess = true;
             } else {
                 detectSuccess = false;
             }
         }
-        
+
+
         if (useRegressor2 && detectSuccess) {
-            shapeToImage = dest::core::estimateSimilarityTransform(dest::core::unitRectangle(), r);
-            s = t[1].predict(img, shapeToImage);
-            r = dest::core::shapeBounds(s);
+            for (int i = 0; i < 2; ++i) {
+                r = dest::core::shapeBounds(s);
+                shapeToImage = dest::core::estimateSimilarityTransform(dest::core::unitRectangle(), r);
+                Eigen::AffineCompact2f tr;
+                tr.setIdentity();
+                tr = Eigen::Translation2f(txToCV * img.cols(), tyToCV * img.rows()) *
+                    Eigen::Translation2f(shapeToImage.translation()) *
+                    Eigen::Scaling(scaleToCV) *
+                    Eigen::Translation2f(-shapeToImage.translation());
+                r = tr * r.colwise().homogeneous();
+
+                shapeToImage = dest::core::estimateSimilarityTransform(dest::core::unitRectangle(), r);
+                s = t[0].predict(img, shapeToImage);
+            }
+            //r = dest::core::shapeBounds(s);
         }
 
     
         dest::util::drawShape(imgCV, s, cv::Scalar(0,0,255));
-        
+        dest::util::drawRect(imgCV, r, cv::Scalar(0, 255, 0));
+        dest::util::drawRect(imgCV, r2, cv::Scalar(0, 0, 255));
         cv::imshow("DEST Tracking", imgCV);
         int key = cv::waitKey(1);
         if (key == 'x')
             done = true;
-        else if (key > 0)
+        else if (key != -1)
             detect = true;
         
     }
