6ba8463ca4f958f55ab1ef526056f8737bfda01b
Christoph Heindl <christoph.heindl@gmail.com>
2016-01-21 07:29:38 +0100

    Changing license of DEST to three-clause BSD

--- inc/dest/io/database_io.h
+++ inc/dest/io/database_io.h
@@ -1,20 +1,11 @@
 /**
- This file is part of Deformable Shape Tracking (DEST).
- 
- Copyright Christoph Heindl 2015
- 
- Deformable Shape Tracking is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
- 
- Deformable Shape Tracking is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
- 
- You should have received a copy of the GNU General Public License
- along with Deformable Shape Tracking. If not, see <http://www.gnu.org/licenses/>.
+    This file is part of Deformable Shape Tracking (DEST).
+
+    Copyright(C) 2015/2016 Christoph Heindl
+    All rights reserved.
+
+    This software may be modified and distributed under the terms
+    of the BSD license.See the LICENSE file for details.
 */
 
 #ifndef DEST_DATABASE_IO_H
