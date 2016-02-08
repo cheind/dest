3a2847fec519a4de6ffe37cc1a275847f5f3b53c
Christoph Heindl <christoph.heindl@gmail.com>
2016-02-08 11:20:29 +0100

    Adding empty face swap.

---
+++ examples/dest_face_swap.cpp
@@ -0,0 +1,21 @@
+/**
+This file is part of Deformable Shape Tracking (DEST).
+
+Copyright(C) 2015/2016 Christoph Heindl
+All rights reserved.
+
+This software may be modified and distributed under the terms
+of the BSD license.See the LICENSE file for details.
+*/
+
+#include <dest/dest.h>
+#include <opencv2/opencv.hpp>
+#include <tclap/CmdLine.h>
+
+/**
+    Swap faces in images.
+*/
+int main(int argc, char **argv)
+{
+    return 0;
+}
